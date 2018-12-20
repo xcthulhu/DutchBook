ec067ebb9b5f9bb9cdfc4a32cc56205c7ad73e0c
Matthew Doty <matthew.wampler.doty@gmail.com>
2018-12-20 09:08:02 -0500

    Cleaning up whitespace, removing dependency on 'Real' (which is builtin)

--- Probability/Logical/Weakly_Additive/Weakly_Additive_Logical_Probability.thy
+++ Probability/Logical/Weakly_Additive/Weakly_Additive_Logical_Probability.thy
@@ -1,37 +1,36 @@
 theory Weakly_Additive_Logical_Probability
-  imports "../../../Logic/Proof/Classical/Classical_Propositional_Connectives" 
-          Real
+  imports "../../../Logic/Proof/Classical/Classical_Propositional_Connectives"
 begin
 
-sledgehammer_params [smt_proofs = false]  
-  
+sledgehammer_params [smt_proofs = false]
+
 class Weakly_Additive_Logical_Probability = Classical_Propositional_Logic +
   fixes Pr :: "'a \<Rightarrow> real"
   assumes Non_Negative: "Pr \<phi> \<ge> 0"
   assumes Unity: "\<turnstile> \<phi> \<Longrightarrow> Pr \<phi> = 1"
   assumes Additivity: "\<turnstile> \<sim> (\<phi> \<sqinter> \<psi>) \<Longrightarrow> Pr (\<phi> \<squnion> \<psi>) = Pr \<phi> + Pr \<psi>"
-    
+
 lemma (in Weakly_Additive_Logical_Probability) Alternate_Additivity:
   "\<turnstile> \<phi> \<rightarrow> \<psi> \<rightarrow> \<bottom> \<Longrightarrow> Pr (\<phi> \<squnion> \<psi>) = Pr \<phi> + Pr \<psi>"
-  by (metis Additivity 
-            Double_Negation_converse 
-            Modus_Ponens 
-            conjunction_def 
+  by (metis Additivity
+            Double_Negation_converse
+            Modus_Ponens
+            conjunction_def
             negation_def)
-    
+
 lemma (in Weakly_Additive_Logical_Probability) falsum_zero_probability:
   "Pr \<bottom> = 0"
-  by (metis add_cancel_left_right 
-            Additivity 
-            Ex_Falso_Quodlibet 
-            Unity 
-            bivalence 
-            conjunction_right_elimination 
+  by (metis add_cancel_left_right
+            Additivity
+            Ex_Falso_Quodlibet
+            Unity
+            bivalence
+            conjunction_right_elimination
             negation_def)
 
 lemma (in Weakly_Additive_Logical_Probability) consistency: "\<not> \<turnstile> \<bottom>"
-  using Unity falsum_zero_probability by auto          
-          
+  using Unity falsum_zero_probability by auto
+
 lemma (in Weakly_Additive_Logical_Probability) falsum_implication_zero_probability:
   "\<turnstile> \<phi> \<rightarrow> \<bottom> \<Longrightarrow> Pr \<phi> = 0"
 proof -
@@ -46,17 +45,17 @@ qed
 
 lemma (in Weakly_Additive_Logical_Probability) complementation:
   "Pr (\<sim> \<phi>) = 1 - Pr \<phi>"
-  by (metis Alternate_Additivity 
-            Unity 
-            bivalence 
-            negation_elimination 
-            add.commute 
+  by (metis Alternate_Additivity
+            Unity
+            bivalence
+            negation_elimination
+            add.commute
             add_diff_cancel_left')
 
 lemma (in Weakly_Additive_Logical_Probability) unity_upper_bound:
   "Pr \<phi> \<le> 1"
   by (metis (no_types) diff_ge_0_iff_ge Non_Negative complementation)
-    
+
 lemma (in Weakly_Additive_Logical_Probability) monotonicity:
   "\<turnstile> \<phi> \<rightarrow> \<psi> \<Longrightarrow> Pr \<phi> \<le> Pr \<psi>"
 proof -
@@ -75,15 +74,15 @@ proof -
     by (simp add: complementation)
   thus ?thesis by linarith
 qed
-           
+
 lemma (in Weakly_Additive_Logical_Probability) biconditional_equivalence:
   "\<turnstile> \<phi> \<leftrightarrow> \<psi> \<Longrightarrow> Pr \<phi> = Pr \<psi>"
-  by (meson eq_iff 
-            Modus_Ponens 
-            biconditional_left_elimination 
-            biconditional_right_elimination 
+  by (meson eq_iff
+            Modus_Ponens
+            biconditional_left_elimination
+            biconditional_right_elimination
             monotonicity)
-          
+
 lemma (in Weakly_Additive_Logical_Probability) sum_rule:
   "Pr (\<phi> \<squnion> \<psi>) + Pr (\<phi> \<sqinter> \<psi>) = Pr \<phi> + Pr \<psi>"
 proof -
@@ -92,7 +91,7 @@ proof -
     have "\<forall> \<MM>. \<MM> \<Turnstile>\<^sub>p\<^sub>r\<^sub>o\<^sub>p (\<^bold>\<langle>\<phi>\<^bold>\<rangle> \<squnion> \<^bold>\<langle>\<psi>\<^bold>\<rangle>) \<leftrightarrow> (\<^bold>\<langle>\<phi>\<^bold>\<rangle> \<squnion> \<^bold>\<langle>\<psi>\<^bold>\<rangle> \<setminus> (\<^bold>\<langle>\<phi>\<^bold>\<rangle> \<sqinter> \<^bold>\<langle>\<psi>\<^bold>\<rangle>))"
       unfolding Classical_Propositional_Logic_class.subtraction_def
                 Minimal_Logic_With_Falsum_class.negation_def
-                Classical_Propositional_Logic_class.biconditional_def 
+                Classical_Propositional_Logic_class.biconditional_def
                 Classical_Propositional_Logic_class.conjunction_def
                 Classical_Propositional_Logic_class.disjunction_def
       by simp
@@ -104,7 +103,7 @@ proof -
     have "\<forall> \<MM>. \<MM> \<Turnstile>\<^sub>p\<^sub>r\<^sub>o\<^sub>p \<^bold>\<langle>\<phi>\<^bold>\<rangle> \<rightarrow> (\<^bold>\<langle>\<psi>\<^bold>\<rangle> \<setminus> (\<^bold>\<langle>\<phi>\<^bold>\<rangle> \<sqinter> \<^bold>\<langle>\<psi>\<^bold>\<rangle>)) \<rightarrow> \<bottom>"
       unfolding Classical_Propositional_Logic_class.subtraction_def
                 Minimal_Logic_With_Falsum_class.negation_def
-                Classical_Propositional_Logic_class.biconditional_def 
+                Classical_Propositional_Logic_class.biconditional_def
                 Classical_Propositional_Logic_class.conjunction_def
                 Classical_Propositional_Logic_class.disjunction_def
       by simp
@@ -118,7 +117,7 @@ proof -
     have "\<forall> \<MM>. \<MM> \<Turnstile>\<^sub>p\<^sub>r\<^sub>o\<^sub>p \<^bold>\<langle>\<psi>\<^bold>\<rangle> \<leftrightarrow> (\<^bold>\<langle>\<psi>\<^bold>\<rangle> \<setminus> (\<^bold>\<langle>\<phi>\<^bold>\<rangle> \<sqinter> \<^bold>\<langle>\<psi>\<^bold>\<rangle>) \<squnion> (\<^bold>\<langle>\<phi>\<^bold>\<rangle> \<sqinter> \<^bold>\<langle>\<psi>\<^bold>\<rangle>))"
       unfolding Classical_Propositional_Logic_class.subtraction_def
                 Minimal_Logic_With_Falsum_class.negation_def
-                Classical_Propositional_Logic_class.biconditional_def 
+                Classical_Propositional_Logic_class.biconditional_def
                 Classical_Propositional_Logic_class.conjunction_def
                 Classical_Propositional_Logic_class.disjunction_def
       by auto
@@ -142,7 +141,7 @@ proof -
     have "\<forall> \<MM>. \<MM> \<Turnstile>\<^sub>p\<^sub>r\<^sub>o\<^sub>p \<^bold>\<langle>\<phi>\<^bold>\<rangle> \<leftrightarrow> ((\<^bold>\<langle>\<phi>\<^bold>\<rangle> \<setminus> \<^bold>\<langle>\<psi>\<^bold>\<rangle>) \<squnion> (\<^bold>\<langle>\<phi>\<^bold>\<rangle> \<sqinter> \<^bold>\<langle>\<psi>\<^bold>\<rangle>))"
       unfolding Classical_Propositional_Logic_class.subtraction_def
                 Minimal_Logic_With_Falsum_class.negation_def
-                Classical_Propositional_Logic_class.biconditional_def 
+                Classical_Propositional_Logic_class.biconditional_def
                 Classical_Propositional_Logic_class.conjunction_def
                 Classical_Propositional_Logic_class.disjunction_def
       by (simp, blast)
@@ -157,7 +156,7 @@ proof -
   proof -
     have "\<forall> \<MM>. \<MM> \<Turnstile>\<^sub>p\<^sub>r\<^sub>o\<^sub>p \<sim>((\<^bold>\<langle>\<phi>\<^bold>\<rangle> \<setminus> \<^bold>\<langle>\<psi>\<^bold>\<rangle>) \<sqinter> (\<^bold>\<langle>\<phi>\<^bold>\<rangle> \<sqinter> \<^bold>\<langle>\<psi>\<^bold>\<rangle>))"
       unfolding Classical_Propositional_Logic_class.subtraction_def
-                Minimal_Logic_With_Falsum_class.negation_def 
+                Minimal_Logic_With_Falsum_class.negation_def
                 Classical_Propositional_Logic_class.conjunction_def
                 Classical_Propositional_Logic_class.disjunction_def
       by simp
@@ -178,7 +177,7 @@ proof -
     by (simp add: sum_rule, simp add: Non_Negative)
   thus ?thesis by linarith
 qed
-  
+
 lemma (in Weakly_Additive_Logical_Probability) arbitrary_disjunction_list_summation_inequality:
   "Pr (\<Squnion> \<Phi>) \<le> (\<Sum>\<phi>\<leftarrow>\<Phi>. Pr \<phi>)"
 proof (induct \<Phi>)
@@ -192,36 +191,36 @@ next
   with Cons have "Pr (\<Squnion> (\<phi> # \<Phi>)) \<le> Pr \<phi> + (\<Sum>\<phi>\<leftarrow>\<Phi>. Pr \<phi>)" by linarith
   then show ?case by simp
 qed
-  
+
 lemma (in Weakly_Additive_Logical_Probability) implication_list_summation_inequality:
   assumes "\<turnstile> \<phi> \<rightarrow> \<Squnion> \<Psi>"
   shows "Pr \<phi> \<le> (\<Sum>\<psi>\<leftarrow>\<Psi>. Pr \<psi>)"
-  using assms arbitrary_disjunction_list_summation_inequality monotonicity order_trans 
-  by blast  
-  
+  using assms arbitrary_disjunction_list_summation_inequality monotonicity order_trans
+  by blast
+
 lemma (in Weakly_Additive_Logical_Probability) arbitrary_disjunction_set_summation_inequality:
   "Pr (\<Squnion> \<Phi>) \<le> (\<Sum>\<phi> \<in> set \<Phi>. Pr \<phi>)"
-  by (metis arbitrary_disjunction_list_summation_inequality 
+  by (metis arbitrary_disjunction_list_summation_inequality
             arbitrary_disjunction_remdups
-            biconditional_equivalence 
-            sum.set_conv_list) 
+            biconditional_equivalence
+            sum.set_conv_list)
 
 lemma (in Weakly_Additive_Logical_Probability) implication_set_summation_inequality:
   assumes "\<turnstile> \<phi> \<rightarrow> \<Squnion> \<Psi>"
   shows "Pr \<phi> \<le> (\<Sum>\<psi> \<in> set \<Psi>. Pr \<psi>)"
-  using assms arbitrary_disjunction_set_summation_inequality monotonicity order_trans 
-  by blast  
+  using assms arbitrary_disjunction_set_summation_inequality monotonicity order_trans
+  by blast
 
 definition (in Classical_Propositional_Logic) Weakly_Additive_Probabilities :: "('a \<Rightarrow> real) set"
-  where "Weakly_Additive_Probabilities = 
+  where "Weakly_Additive_Probabilities =
          {Pr. class.Weakly_Additive_Logical_Probability (\<lambda> \<phi>. \<turnstile> \<phi>) (op \<rightarrow>) \<bottom> Pr }"
 
 definition (in Classical_Propositional_Logic) Binary_Probabilities :: "('a \<Rightarrow> real) set"
-  where "Binary_Probabilities = 
-         {Pr.   class.Weakly_Additive_Logical_Probability (\<lambda> \<phi>. \<turnstile> \<phi>) (op \<rightarrow>) \<bottom> Pr 
+  where "Binary_Probabilities =
+         {Pr.   class.Weakly_Additive_Logical_Probability (\<lambda> \<phi>. \<turnstile> \<phi>) (op \<rightarrow>) \<bottom> Pr
               \<and> (\<forall>x. Pr x = 0 \<or> Pr x = 1)}"
 
-lemma (in Classical_Propositional_Logic) Binary_Probabilities_subset: 
+lemma (in Classical_Propositional_Logic) Binary_Probabilities_subset:
   "Binary_Probabilities \<subseteq> Weakly_Additive_Probabilities"
   unfolding Weakly_Additive_Probabilities_def Binary_Probabilities_def
   by fastforce
@@ -234,8 +233,8 @@ proof -
   have "class.Weakly_Additive_Logical_Probability (\<lambda> \<phi>. \<turnstile> \<phi>) (op \<rightarrow>) \<bottom> ?Pr"
   proof (standard, simp,
          meson assms
-               Formula_Maximally_Consistent_Set_reflection 
-               Maximally_Consistent_Set_def 
+               Formula_Maximally_Consistent_Set_reflection
+               Maximally_Consistent_Set_def
                set_deduction_weaken)
      fix \<phi> \<psi>
      assume "\<turnstile> \<sim> (\<phi> \<sqinter> \<psi>)"
@@ -251,20 +250,20 @@ proof -
                  set_deduction_weaken
                  weak_biconditional_weaken)
      hence "\<phi> \<notin> \<Omega> \<or> \<psi> \<notin> \<Omega>"
-       using assms 
-             Formula_Maximally_Consistent_Set_reflection 
+       using assms
+             Formula_Maximally_Consistent_Set_reflection
              Maximally_Consistent_Set_def
              conjunction_set_deduction_equivalence
-       by meson 
-             
+       by meson
+
      have "\<phi> \<squnion> \<psi> \<in> \<Omega> = (\<phi> \<in> \<Omega> \<or> \<psi> \<in> \<Omega>)"
-       by (metis \<open>\<phi> \<sqinter> \<psi> \<notin> \<Omega>\<close> 
-                 assms 
-                 Formula_Maximally_Consistent_Set_implication 
-                 Maximally_Consistent_Set_def 
-                 conjunction_def 
+       by (metis \<open>\<phi> \<sqinter> \<psi> \<notin> \<Omega>\<close>
+                 assms
+                 Formula_Maximally_Consistent_Set_implication
+                 Maximally_Consistent_Set_def
+                 conjunction_def
                  disjunction_def)
-     show "(if \<phi> \<squnion> \<psi> \<in> \<Omega> then (1::real) else 0) 
+     show "(if \<phi> \<squnion> \<psi> \<in> \<Omega> then (1::real) else 0)
          = (if \<phi> \<in> \<Omega> then (1::real) else 0) + (if \<psi> \<in> \<Omega> then (1::real) else 0)"
      proof (cases "\<phi> \<squnion> \<psi> \<in> \<Omega>")
        case True
@@ -273,21 +272,21 @@ proof -
        proof (cases "\<phi> \<in> \<Omega>")
          case True
          hence "\<psi> \<notin> \<Omega>"
-           using \<open>\<phi> \<notin> \<Omega> \<or> \<psi> \<notin> \<Omega>\<close> 
+           using \<open>\<phi> \<notin> \<Omega> \<or> \<psi> \<notin> \<Omega>\<close>
            by blast
          have "(if \<phi> \<squnion> \<psi> \<in> \<Omega> then 1 else 0) = (1::real)" using \<diamondsuit> by simp
          also have "... = 1 + (0::real)" by linarith
-         also have "... = (if \<phi> \<in> \<Omega> then 1 else 0) + (if \<psi> \<in> \<Omega> then 1 else 0)" 
+         also have "... = (if \<phi> \<in> \<Omega> then 1 else 0) + (if \<psi> \<in> \<Omega> then 1 else 0)"
            using \<open>\<psi> \<notin> \<Omega>\<close> \<open>\<phi> \<in> \<Omega>\<close> by simp
          finally show ?thesis .
        next
          case False
          hence "\<psi> \<in> \<Omega>"
-           using \<open>\<phi> \<squnion> \<psi> \<in> \<Omega>\<close> \<open>(\<phi> \<squnion> \<psi> \<in> \<Omega>) = (\<phi> \<in> \<Omega> \<or> \<psi> \<in> \<Omega>)\<close> 
+           using \<open>\<phi> \<squnion> \<psi> \<in> \<Omega>\<close> \<open>(\<phi> \<squnion> \<psi> \<in> \<Omega>) = (\<phi> \<in> \<Omega> \<or> \<psi> \<in> \<Omega>)\<close>
            by blast
          have "(if \<phi> \<squnion> \<psi> \<in> \<Omega> then 1 else 0) = (1::real)" using \<diamondsuit> by simp
          also have "... = (0::real) + 1" by linarith
-         also have "... = (if \<phi> \<in> \<Omega> then 1 else 0) + (if \<psi> \<in> \<Omega> then 1 else 0)" 
+         also have "... = (if \<phi> \<in> \<Omega> then 1 else 0) + (if \<psi> \<in> \<Omega> then 1 else 0)"
            using \<open>\<psi> \<in> \<Omega>\<close> \<open>\<phi> \<notin> \<Omega>\<close> by simp
          finally show ?thesis .
        qed
@@ -297,7 +296,7 @@ proof -
          using \<open>(\<phi> \<squnion> \<psi> \<in> \<Omega>) = (\<phi> \<in> \<Omega> \<or> \<psi> \<in> \<Omega>)\<close> by blast+
        ultimately show ?thesis by simp
      qed
-  qed          
+  qed
   thus ?thesis
     unfolding Binary_Probabilities_def
     by simp
@@ -309,23 +308,23 @@ lemma (in Classical_Propositional_Logic) arbitrary_disjunction_exclusion_MCS:
 proof (induct \<Psi>)
   case Nil
   then show ?case
-    using assms 
-          Formula_Consistent_def 
-          Formula_Maximally_Consistent_Set_def 
-          Maximally_Consistent_Set_def 
-          set_deduction_reflection 
-    by (simp, blast) 
+    using assms
+          Formula_Consistent_def
+          Formula_Maximally_Consistent_Set_def
+          Maximally_Consistent_Set_def
+          set_deduction_reflection
+    by (simp, blast)
 next
   case (Cons \<psi> \<Psi>)
   have "\<Squnion> (\<psi> # \<Psi>) \<notin> \<Omega> = (\<psi> \<notin> \<Omega> \<and> \<Squnion> \<Psi> \<notin> \<Omega>)"
     by (simp add: disjunction_def,
-        meson assms 
-              Formula_Consistent_def 
+        meson assms
+              Formula_Consistent_def
               Formula_Maximally_Consistent_Set_def
               Formula_Maximally_Consistent_Set_implication
-              Maximally_Consistent_Set_def 
+              Maximally_Consistent_Set_def
               set_deduction_reflection)
   thus ?case using Cons.hyps by simp
 qed
-    
+
 end
